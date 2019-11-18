0a013859448fec4de2d93b766e4de01abffaabaf
brain-hack <tyoung1@nate.com>
2019-11-18 21:30:29 +0900

    add func print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+import sys
+
 def print_message():
     message = "world"
     print("hello {}".format(message))
+
+def print_git():
+    message = "from the hell"
+
 
 if __name == "__main__":
     print_message()
